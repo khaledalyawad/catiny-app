aa283f28a43b06098990a8b36036dff91b227478
yuvy tung <yuvytung@gmail.com>
2021-10-21 01:24:42 +0700

    expo -> react-native

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,23 @@
-module.exports = function (api) {
-  api.cache(true);
-  return {
-    presets: ['babel-preset-expo'],
-  };
+// module.exports = function (api) {
+//   api.cache(true);
+//   return {
+//     presets: ['babel-preset-expo'],
+//   };
+// };
+module.exports = {
+  presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      '@babel/plugin-proposal-decorators',
+      {
+        legacy: true,
+      },
+    ],
+    ['@babel/plugin-proposal-optional-catch-binding'],
+  ],
+  env: {
+    production: {
+      plugins: ['transform-remove-console'],
+    },
+  },
 };
